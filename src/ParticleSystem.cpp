794a0fdb6e5bc0e3da49f0548a5fc64f945f7f6c
Rosendo Martinez <rosendomartinez270@gmail.com>
2025-02-05 20:46:40 -0600

    fading particles

--- src/ParticleSystem.cpp
+++ src/ParticleSystem.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <random>
 
+const float PARTICLE_LIFETIME_SEC = 1.0;
 
 float randomFloat(float min, float max) {
     // Copied and pasted from chatgpt.
@@ -18,8 +19,6 @@ ParticleSystem::ParticleSystem()
 
 void ParticleSystem::createParticle(glm::vec3 pos, glm::vec3 vel)
 {
-    const float PARTICLE_LIFETIME_SEC = 1.0;
-
     Particle p;
     p.pos = pos;
     p.vel = vel;
@@ -34,7 +33,7 @@ void ParticleSystem::createParticle(glm::vec3 pos, glm::vec3 vel)
     p.vel.z += randomFloat(-7.0, 7.0);
 
     // random grey scale color, and random opacity
-    p.color = glm::vec4(glm::vec3(randomFloat(0.7f, 1.0f)), randomFloat(0.2f, 0.6f));
+    p.color = glm::vec4(glm::vec3(randomFloat(0.7f, 1.0f)), 1.0f);
 
     particles.push_back(p);
 }
@@ -53,5 +52,6 @@ void ParticleSystem::update(float dt)
         p.pos += p.vel * dt;
         p.vel.y += GRAVITY * dt;
         p.life -= dt;
+        p.color.w = p.life / PARTICLE_LIFETIME_SEC;
     }
 }
